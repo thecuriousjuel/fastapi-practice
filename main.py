db54ae6c6eda330825704eebbca9a72ffe8bd68d
thecuriousjuel <juelbasak@gmail.com>
2023-07-28 02:31:13 +0530

    Learned about query parameter and path parameter

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# To start the fast API, run the below command
+# uvicorn main:app --reload 
+
+from typing import Optional
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -10,3 +14,25 @@ def index():
 @app.get('/about')
 def about():
     return {'data' : 'About Page'}
+
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'unpublished blog'}
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id: int):
+    return {'data' : {'comments' : id}}
+
+
+@app.get('/blog')
+def get_blogs(limit: int=10, published: bool=True, sort: Optional[str]=None):
+    if published:
+        return {'data' : limit, 'published': published, 'sort': sort}
+    return {'data' : limit, 'published': published, 'sort': sort}
