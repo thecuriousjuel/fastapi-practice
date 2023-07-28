e5a4b4651b2fa76db5cdd59dc500b8a89b9644fe
thecuriousjuel <juelbasak@gmail.com>
2023-07-28 20:30:53 +0530

    Get data from tables

--- blog/main.py
+++ blog/main.py
@@ -1,14 +1,37 @@
 # uvicorn main:app --reload
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from schemas import Blog
 import models
-from database import engine
+from database import engine, session_local
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
+def get_db():
+    db = session_local()
+    try:
+        yield db
+    finally:
+        db.close()
+
 @app.post('/blog')
-def create(request: Blog):
-    return request
+def create(request: Blog, db: Session=Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+
+    return new_blog
+
+@app.get('/blog')
+def get_blogs(db: Session=Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+@app.get('/blog/{id}')
+def get_blogs_via_id(id: int, db: Session=Depends(get_db)):
+    blogs = db.query(models.Blog).filter(models.Blog.id == id).first()
+    return blogs
